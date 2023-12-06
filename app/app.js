2fd60333f7ab87eceb329e6e9de9d597c00be2a9
duvanmonroy <duvanmonroy1127@gmail.com>
2023-12-05 20:21:19 -0500

    jhonv cambios puertos

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const app = express();
 
-// Configuración de CORS para permitir solicitudes desde el frontend (Vercel)
+// Configuración de CORS
 const corsOptions = {
   origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -17,7 +17,6 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-
 app.use(express.json());
 
 // Rutas para el primer servidor
@@ -25,30 +24,9 @@ app.get('/user', verifyToken, controllers.getUserById);
 app.post('/register', controllers.register);
 app.post('/login', controllers.login);
 
-// Conexión a la base de datos para el primer servidor
-connectToDatabase();
-
-// Inicio del servidor en el puerto especificado en el archivo .env o en el puerto 5000 por defecto
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Primer servidor funcionando en el puerto ${PORT}`);
-});
-
-// Crear una segunda instancia de Express para el segundo servidor (Stripe)
-const appStripe = express();
-
-// Configuración de CORS para el segundo servidor (Stripe)
-const corsOptionsStripe = {
-  origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
-  methods: "POST",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-appStripe.use(cors(corsOptionsStripe));
-
 // Rutas para el segundo servidor (Stripe)
-appStripe.use(express.static("public"));
-appStripe.post("/checkout", async (req, res) => {
+app.use(express.static("public"));
+app.post("/checkout", async (req, res) => {
   try {
     // Lógica para el segundo servidor (Stripe)
     const items = req.body.items;
@@ -79,10 +57,13 @@ appStripe.post("/checkout", async (req, res) => {
   }
 });
 
-// Inicio del segundo servidor en el puerto especificado en el archivo .env o en el puerto 4000 por defecto
-const PORT_STRIPE = process.env.PORT_STRIPE || 4000;
-appStripe.listen(PORT_STRIPE, () => {
-  console.log(`Segundo servidor iniciado en el puerto ${PORT_STRIPE}`);
+// Conexión a la base de datos
+connectToDatabase();
+
+// Inicio del servidor en el puerto especificado en el archivo .env o en el puerto 5000 por defecto
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
 
 module.exports = app;
